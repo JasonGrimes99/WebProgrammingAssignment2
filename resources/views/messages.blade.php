b8e349a86c7e647509bcb69204027ee54732f3c0
GiraffeKOOOO <pstepien@hotmail.co.uk>
2019-03-19 16:56:19 +0000

    added Ben's message handler

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,4 +1,29 @@
-<!DOCTYPE html>
+<?php
+
+$numOfMessages = 10;
+
+function messageBuilder($name, $date, $content){
+
+    echo "
+<div class=\"row\">
+
+    <div class='col'>
+        <a href='#'>$name</a>
+    </div>
+
+    <div class='col'>
+        <p>$content</p>
+    </div>
+
+    <div class='col'>
+        <p>$date</p>
+    </div>
+
+</div>
+";
+}
+?>
+        <!DOCTYPE html>
 
 <html lang="en">
 <head>
@@ -70,50 +95,22 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <h2>Message Name</h2>
-                </div>
-                <div class="col">
-                    <h2>Date and Time Received</h2>
-                </div>
-                <div class="col">
                     <h2>Sender</h2>
                 </div>
-            </div>
-            <div class="row">
                 <div class="col">
-                    <a href="#">First Message</a>
+                    <h2></h2>
                 </div>
                 <div class="col">
-                    <p>Date and Time received</p>
-                </div>
-                <div class="col">
-                    <p>Sender</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <a href="#">Second Message</a>
-                </div>
-                <div class="col">
-                    <p>Date and Time received</p>
-                </div>
-                <div class="col">
-                    <p>Sender</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <a href="#">Third Message</a>
-                </div>
-                <div class="col">
-                    <p>Date and Time received</p>
-                </div>
-                <div class="col">
-                    <p>Sender</p>
+                    <h2>Received</h2>
                 </div>
             </div>
         </div>
+        <?php
+        for($i=0; $i <= $numOfMessages; $i++){
+            messageBuilder("Ben Spencer", "27/10/2016 15:00:00", "Hello, This is my generated message, also hello world");}
+        ?>
     </div>
+</div>
 </div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
